762166bb05b52dbf282365ff5d859867a06fad03
Congyu ZHANG <congyu.zhang@student.bodwell.edu>
2018-01-20 12:10:35 -0800

    Initial commit

---
+++ src/Basics.java
@@ -0,0 +1,55 @@
+/**
+ * Execises to practice the basic syntax
+ */
+public class Basics {
+
+    public static void main(String[] args) {
+        // TODO: Write a Java program to print the sum of two numbers.
+        int luka =4;
+        int vdv=6;
+        int result= luka+vdv;
+        System.out.println("sum is "+result);
+
+
+        // TODO: Write a Java program to divide two numbers and print on the screen. Use both integers and doubles.
+         int c=8;
+         int u=10;
+         int result1=c/u;
+         System.out.println("quation is "+result1);
+         double i=5;
+         double k=9;
+         double result2=i/k;
+         System.out.println("quation is "+result2);
+
+        // TODO: Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
+         int sum=c+u;
+         System.out.println("sum is "+sum);
+         int sum1=c*u;
+         System.out.println("multiplication is"+sum1);
+         int y=7;
+         int o=6;
+         int result3=y-o;
+         System.out.println("subtract is "+result3);
+         double sum3=i/k;
+         System.out.println("quotation "+sum3);
+         int sum4=luka%vdv;
+         System.out.println("reminder "+sum4);
+
+
+
+
+
+        // TODO: Write a Java program to swap two variables. Print the before and after switching.
+        int alpha=14;
+        int bravo=15;
+        int charlie;
+        System.out.println("the old alpha is "+alpha);
+        System.out.println("the old bravo is "+bravo);
+        charlie = alpha;
+        alpha = bravo;
+        bravo = charlie;
+        System.out.println("the new alpha is "+alpha);
+        System.out.println("the new bravo is "+bravo);
+
+    }
+}
